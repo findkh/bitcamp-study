c9a7d281f584f5265137f1e8474341cde1b65b32
KyoungHyunPark <findkh@nate.com>
2021-12-13 18:15:31 +0900

    211213

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -1,42 +1,65 @@
 package com.eomcs.mylist;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //클라이언트 요청을 처리하는 역할 저장하면 import 자동으로 됨
+@RestController //이 클래스가 클라이언트 요청을 처리 담당자임을 표시한다
+//이 표시(애노테이션)가 붙어 있어야만 스프링부트가 이 클래스를 인식한다.
 public class ContactController {
 
-  //인스턴스변수
-  // -> 모든 인스턴스 메서드가 공유한다.
-  String[] contacts = new String[5]; 
+  String[] contacts = new String[5];
   int size = 0;
 
-  @GetMapping("/contact/list")
+  @RequestMapping("/contact/list")
   public Object list() {
-    String[] records = new String[size];
-    for (int i = 0; i < size; i++) {
-      records[i] = contacts[i];
+    String[] arr = new String[size]; //배열에 저장된 값만 복사할 새 배열을 만든다.
+    for (int i =0; i < size; i++) {
+      arr[i] = contacts[i]; //전체 배열에서 값이 들어 있는 항목만 복사한다.
     }
-    return records;
+    return arr; // 복사한 항목들을 담고 있는 새 배열을 리턴한다. 
   }
 
-  @GetMapping("/contact/add")
+  @RequestMapping("/contact/add")
   public Object add(String name, String email, String tel, String company) {
-    //int temp = size;
-    //size = size + 1;
-    //contacts[tempt]
-    contacts[size++] = name + "," + email+ "," + tel + "," + company;
+    String contact = name + "," + email + "," + tel + "," + company;
+    contacts[size++] = contact;
     return size;
   }
 
-  @GetMapping("/contact/get")
+  @RequestMapping("/contact/get")
   public Object get(String email) {
     for (int i = 0; i < size; i++) {
-      if (email.equals(contacts[i].split(",")[1])) {
+      if (contacts[i].split(",")[1].equals(email)) { 
         return contacts[i];
       }
     }
     return "";
   }
 
+  @RequestMapping("/contact/update")
+  public Object update(String name, String email, String tel, String company) {
+    String contact = name + "," + email + "," + tel + "," + company;
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].split(",")[1].equals(email)) { 
+        contacts[i] = contact;
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  @RequestMapping("/contact/delete")
+  public Object delete(String email) {
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].split(",")[1].equals(email)) { // 예) "u1@test.com"
+        // 현재 위치의 다음 항목에서 배열 끝까지 반복하며 앞으로 값을 당겨온다.
+        for (int j = i + 1; j < size; j++) {
+          contacts[j - 1] = contacts[j];
+        }
+        size--;
+        return 1;
+      }
+    }
+    return 0;
+  }
 }
