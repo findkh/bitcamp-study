30cf6ebc2701f256945e488b0c5397a55be641e8
KyoungHyunPark <findkh@nate.com>
2022-03-29 17:54:11 +0900

    220329

---
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.eomcs.mylist.service;
+
+import com.eomcs.mylist.domain.Member;
+
+public interface MemberService {
+
+  int add(Member member);
+
+  //  List<Board> list();
+  //
+  //  Board get(int no);
+  //
+  //  int update(Board board);
+  //
+  //  int delete(int no);
+
+}
