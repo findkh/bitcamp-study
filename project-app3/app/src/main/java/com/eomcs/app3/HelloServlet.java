5733f6bedcfa4f5b993f405e524a2c66fdf4d246
KyoungHyunPark <findkh@nate.com>
2022-04-11 11:32:41 +0900

    .

--- project-app3/app/src/main/java/com/eomcs/app3/HelloServlet.java
+++ project-app3/app/src/main/java/com/eomcs/app3/HelloServlet.java
@@ -12,21 +12,28 @@ import javax.servlet.annotation.WebServlet;
 //서블릿 컨테이너가 실행할 클래스를 만드려면
 //Servlet API 규칙에 따라 작성해야 한다.
 //
-@WebServlet("/hello")
+@WebServlet("/hello") //서블릿 컨테이너에게 이 클래스가 /hello 요청을 처리하는 서블릿임을 알려준다.
 public class HelloServlet implements Servlet {
+
+  ServletConfig config;
 
   @Override
   public void init(ServletConfig config) throws ServletException {
+    System.out.println("init() 호출");
+    this.config = config;
   }
 
   @Override
   public ServletConfig getServletConfig() {
-    return null;
+    System.out.println("getServletConfig() 호출");
+    return this.config;
   }
 
   @Override
   public void service(ServletRequest req, ServletResponse res)
       throws ServletException, IOException {
+
+    System.out.println("service() 호출");
 
     //HTTP 클라이언트가 name이라는 이름으로 보내 온 파라미터 값을 읽는다.
     String name = req.getParameter("name");
@@ -43,11 +50,13 @@ public class HelloServlet implements Servlet {
 
   @Override
   public String getServletInfo() {
-    return null;
+    System.out.println("getServletInfo() 호출");
+    return "HelloServlet";
   }
 
   @Override
   public void destroy() {
+    System.out.println("destroy() 호출");
   }
 
 }
